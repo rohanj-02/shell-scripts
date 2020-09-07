4ae5ca3345f84368b6e18be674c60ebf6e7d783b
Rohan Jain <rohan19095@iiitd.ac.in>
2020-09-08 00:12:39 +0530

    Tutorials Added in Course Open

--- course_open/course_open.sh
+++ course_open/course_open.sh
@@ -16,6 +16,18 @@ firefox.exe $CONST_URLS $AP_LINKS
 elif [[ "$var" == "ff" || "$var" == "fof" ]]
 then
 firefox.exe $FF_LINKS $CONST_URLS
+elif [ "$var" == "snst" ]
+then
+firefox.exe $CONST_URLS $SNS_TUT_LINKS 
+elif [ "$var" == "apt" ]
+then
+firefox.exe $CONST_URLS $AP_TUT_LINKS 
+elif [ "$var" == "dmt" ]
+then
+firefox.exe $CONST_URLS $DM_TUT_LINKS 
+elif [ "$var" == "ost" ]
+then
+firefox.exe $CONST_URLS $OS_TUT_LINKS 
 else
 echo "No such course!"
 fi
